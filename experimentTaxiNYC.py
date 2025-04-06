8e7c00c2c930f1ecf6db020c8ce71bead09fc509
FelixxLuo <15574654+felixxluo@user.noreply.gitee.com>
2025-04-06 11:24:01 +0200

    Upload main function

--- experimentTaxiNYC.py
+++ experimentTaxiNYC.py
@@ -1,9 +1,7 @@
 import sys
 import logging
 from torch.utils.data import TensorDataset, DataLoader
-
-sys.path.append('../')
-from data.TaxiNYC import f
+from data.data_preparation import get_sequential_data
 from model.STResNet import STResNet
 import torch
 import numpy as np
@@ -14,20 +12,18 @@ if gpu_available:
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format=' %(levelname)s - %(message)s')
-    nb_epoch = 100  # number of epoch at training stage
-    batch_size = 32  # batch size
-    T = 24  # number of time intervals in one day
-    len_closeness = 3  # length of closeness dependent sequence
-    len_period = 1  # length of period dependent sequence
-    len_trend = 1  # length of trend dependent sequence
-    days_test = 7 * 4
-    len_test = T * days_test
-    map_height, map_width = 32, 32  # grid size
-    nb_flow = 2
-    lr = 0.0002  # learning rate
-    nb_residual_unit = 4
+    nb_epoch = 100    # number of epoch at training stage
+    batch_size = 32    # batch size
+    T = 24    # number of time intervals in one day
+    len_closeness = 3    # length of closeness dependent sequence
+    len_period = 1    # length of period dependent sequence
+    len_trend = 1    # length of trend dependent sequence
+    map_height, map_width = 32, 32    # grid size
+    nb_flow = 2    # pickup counts & dropoff counts
+    lr = 0.0002    # learning rate
+    nb_residual_unit = 4    # number of residual unit
 
-    X_train, Y_train, X_test, Y_test, mmn, external_dim = f()
+    X_train, Y_train, X_test, Y_test, mmn, external_dim = get_sequential_data(len_closeness, len_period, len_trend)
 
     for i in range(4):
         print(np.min(X_train[i]))
@@ -45,6 +41,7 @@ if __name__ == '__main__':
         epoches=nb_epoch,
         batch_size=batch_size,
         len_closeness=len_closeness,
+        len_period=len_period,
         len_trend=len_trend,
         external_dim=external_dim,
         map_heigh=map_height,
